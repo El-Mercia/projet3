a9f03b0268dd2d3c14524d81d2f0f2ea0ceadf40
Mercia <merciarandrianome@gmail.com>
2020-03-20 16:11:45 +0100

    Rien ne va pour le moment

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const fs = require("fs");
+
+const app = express();
+
+app.listen(3000, () => {
+	console.log("SERVER STARTED...");
+});
+
+app.get("/", (req, res) => {
+	console.log("access to / path");
+	const html = fs
+		.readFileSync("./public/index.html")
+		.toString("utf8");
+	
+	res.send(html);
+});
+
+app.get("/about", (req, res) => {
+	res.send("about");
+}); 
